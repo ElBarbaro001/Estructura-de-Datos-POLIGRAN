524ae9633d45b4eaa099b559a14fd106f39e3b3d
proot <proot@machine>
2020-04-14 23:59:31 -0500

    Ingresando datos a los nodos

--- Edatos/src/arbolbinario/interfaz.java
+++ Edatos/src/arbolbinario/interfaz.java
@@ -4,6 +4,9 @@ public class interfaz {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		binario binraiz = new binario(8);
+		binario binizquierdo = new binario(5);
+		binario binderecho = new binario(3);
 
 	}
 
