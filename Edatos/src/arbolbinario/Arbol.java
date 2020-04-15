b7ed859a9086b983e401b5da09c56f5d6869c4c4
proot <proot@machine>
2020-04-14 23:53:02 -0500

    mmm

--- Edatos/src/arbolbinario/Arbol.java
+++ Edatos/src/arbolbinario/Arbol.java
@@ -1,5 +1,11 @@
 package arbolbinario;
 
 public class Arbol {
-	
+	binario raiz;
+	public Arbol() {
+		raiz=null;
+	}
+	public Arbol(binario raiz) {
+		this.raiz=raiz;	
+	}
 }
---
+++ Edatos/src/arbolbinario/interfaz.java
@@ -0,0 +1,10 @@
+package arbolbinario;
+
+public class interfaz {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
