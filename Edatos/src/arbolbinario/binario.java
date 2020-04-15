0195d41934b75d5389290cc45149579c638793f8
proot <proot@machine>
2020-04-14 23:40:20 -0500

    Se crea la clase binario

---
+++ Edatos/src/arbolbinario/binario.java
@@ -0,0 +1,14 @@
+package arbolbinario;
+
+public class binario {
+		int valor;
+		binario izquierdo;
+		binario derecho;
+		
+		public binario(int valor) {
+			this.valor=valor;
+			izquierdo=null;
+			derecho=null;
+		}
+		
+}
