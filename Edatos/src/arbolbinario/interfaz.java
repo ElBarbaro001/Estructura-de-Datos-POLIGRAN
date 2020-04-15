9bd10b3311038a57ac76cdc8844578a8280a9973
proot <proot@machine>
2020-04-15 15:39:19 -0500

    Cambio

--- Edatos/src/arbolbinario/interfaz.java
+++ Edatos/src/arbolbinario/interfaz.java
@@ -7,7 +7,6 @@ public class interfaz {
 		binario binraiz = new binario(8);
 		binario binizquierdo = new binario(5);
 		binario binderecho = new binario(3);
-		binario binderecho2 = new binario(3);
 
 	}
 
