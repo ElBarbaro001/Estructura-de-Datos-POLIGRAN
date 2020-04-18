7003d312c48dc5723bdfd71b45ce08b621c1e345
proot <proot@machine>
2020-04-18 18:45:08 -0500

    Crear Arbol Binario

--- Edatos/src/codigo_cartilla/ppal.java
+++ Edatos/src/codigo_cartilla/ppal.java
@@ -4,9 +4,11 @@ public class ppal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		VEDArbin<String> arbolnull=new VEDArbin<String>();
-		VEDArbin<String> arbol1=new VEDArbin<String>("Nodo Padre",arbolnull,arbolnull);
+		VEDArbin<String> vacio=new VEDArbin<String>();
+		VEDArbin<String> arbol1=new VEDArbin<String>("Raiz",vacio,vacio);
+		VEDArbin<String> arbol2=new VEDArbin<String>("Lado Derecho",vacio,vacio);
 		System.out.println(arbol1.getVal());
+		System.out.println(arbol2.getVal());
 	}
 
 }
