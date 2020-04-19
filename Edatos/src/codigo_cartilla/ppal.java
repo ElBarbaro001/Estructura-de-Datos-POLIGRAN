ad688d1462f97474a8a281957491c8e8c3427ba9
proot <proot@machine>
2020-04-18 19:28:31 -0500

    Agregar datos o valores al arbol

--- Edatos/src/codigo_cartilla/ppal.java
+++ Edatos/src/codigo_cartilla/ppal.java
@@ -4,11 +4,16 @@ public class ppal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		VEDArbin<String> vacio=new VEDArbin<String>();
-		VEDArbin<String> arbol1=new VEDArbin<String>("Raiz",vacio,vacio);
-		VEDArbin<String> arbol2=new VEDArbin<String>("Lado Derecho",vacio,vacio);
+		VEDArbin<Integer> vacio=new VEDArbin<Integer>();
+		VEDArbin<Integer> arbol1=new VEDArbin<Integer>(9,vacio,vacio);
+		//public VEDArbin(E pVal,VEDArbin<E> pDer, VEDArbin<E> pIzq)
 		System.out.println(arbol1.getVal());
+		VEDArbin<Integer> arbol2=new VEDArbin<Integer>(20,vacio,vacio);
 		System.out.println(arbol2.getVal());
+		VEDArbin<Integer> arbol3=new VEDArbin<Integer>(30,vacio,vacio);
+		System.out.println(arbol3.getVal());
+		System.out.println(arbol1.esVacio());
+		System.out.println(arbol1.peso());
+		System.out.println(arbol1.altura());
 	}
-
 }
