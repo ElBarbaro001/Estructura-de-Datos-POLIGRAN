7d2d4412025bfe42af0fc092a203d6c0f61afdc4
proot <proot@machine>
2020-04-22 20:31:37 -0500

    root

--- Edatos/src/codigo_cartilla/VEDArbin.java
+++ Edatos/src/codigo_cartilla/VEDArbin.java
@@ -1,38 +1,107 @@
 package codigo_cartilla;
-
+import java.util.*;
 
 public class VEDArbin<E>{//Figura 11 Declaracion de la clase VEDArbin<E>
 	
-	protected E val;
-	protected VEDArbin<E> izq;
-	protected VEDArbin<E> der;
+	protected E val;//raiz
+	protected VEDArbin<E> der;//rama del lado derecho
+	protected VEDArbin<E> izq;// rama del lado izquierdo
 	
-	public VEDArbin() {//figura 12 Metodos constructores de la clase VEDArbin<E>
+	public VEDArbin() {//figura 12 Metodos constructores de la clase VEDArbin<E> //Metodo destructor
 		val=null;
-		izq=null;
 		der=null;
+		izq=null;
 	}
 	
-	 public VEDArbin(E pVal,VEDArbin<E> pDer, VEDArbin<E> pIzq) {
-	        if (pVal == null) {
+	 public VEDArbin(E pVal,VEDArbin<E> pDer, VEDArbin<E> pIzq) {//Metodo constructor
+		 val = pVal;
+	     der = pDer;
+	      izq = pIzq;
+		 if (pVal == null) {
 	            throw new NullPointerException("Un arbol vacio debe tener raiz!");
 	        }
 	         if (pDer == null) {
 	            throw new NullPointerException("Un arbol vacio debe tener sub arbol izquierdo!");
 	        }
-	         
 	          if (pIzq == null) {
 	            throw new NullPointerException("Un arbol vacio debe tener sub arbol derecho!");
 	        }
-	        
-	        der = pDer;
-	        izq = pIzq;
-	        val = pVal;
 	    }
 	 
-	 public E getVal() {
+	 public E getVal() {//Imprimir valor de la Raiz
 	        return val;
 	    }
-	
+	 
+	 public VEDArbin<E> getDer() {//Imprimir valor derecho del abol
+	        return der;
+	    }
+	 public VEDArbin<E> getIzq() {//Imprimir valor izquierdo
+	        return izq;
+	    }
+	 
+	 public boolean esVacio(){//Imprimir arbol vacio
+	        return val == null;
+	    }
+	 
+	 public boolean esHoja(){//Un nodo es una hoja si sus dos subárboles son vacíos
+	        if (esVacio()) {
+	            return false;
+	        }
+	        return izq.esVacio() && der.esVacio();
+	    }
+	 
+	 public int peso(){
+	        if(esVacio()){
+	            return 0;
+	        }else{
+	            return 1+izq.peso()+der.peso();
+	        }       
+	    }
+	 
+	 public int altura(){
+	        if(esVacio()){
+	            return 0;
+	            
+	        }else{
+	            return 1+Math.max(izq.altura(), der.altura()) ;
+	        }
+	    }
+	 public int numeroOcurrencias(Object Pobject){
+	        if (esVacio()) {
+	            return 0;
 
+	        }else{
+	            int contadorIzq = izq.numeroOcurrencias(Pobject);
+	            int contadorDer = der.numeroOcurrencias(Pobject);
+	            if (Pobject.equals(val)) {
+	                return 1+contadorIzq+contadorDer;
+	            }else{
+	                return contadorIzq+contadorDer;
+	            }
+
+	        }
+
+	    }
+	 	 //Insertar elementos al arbol
+	 public boolean agregar (E element) {
+		 if(esVacio()) {
+			 val=element;
+			 izq=new VEDArbin<E>();
+			 der=new VEDArbin<E>();
+			 return true;
+		 }
+		 else {
+			 Comparable<E> x=(Comparable<E>) element;
+			 int c=x.compareTo(val);
+			 if(c==0) {
+				 return false;
+			 }
+			 else if (c<0) {
+				 return getIzq().agregar(element);
+			 }
+			 else {
+				 return getDer().agregar(element);
+			 }
+		 }
+	 }
 }
