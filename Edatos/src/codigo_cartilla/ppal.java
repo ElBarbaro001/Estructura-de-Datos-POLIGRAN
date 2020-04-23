7bb9fba7f88d4e3a8d77293083f79f2c2377548d
proot <proot@machine>
2020-04-22 21:17:23 -0500

    root

--- Edatos/src/codigo_cartilla/ppal.java
+++ Edatos/src/codigo_cartilla/ppal.java
@@ -1,19 +1,17 @@
 package codigo_cartilla;
-
+import java.util.*;
 public class ppal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		VEDArbin<Integer> vacio=new VEDArbin<Integer>();
-		VEDArbin<Integer> arbol1=new VEDArbin<Integer>(9,vacio,vacio);
-		//public VEDArbin(E pVal,VEDArbin<E> pDer, VEDArbin<E> pIzq)
-		System.out.println(arbol1.getVal());
-		VEDArbin<Integer> arbol2=new VEDArbin<Integer>();
-		System.out.println(arbol2.getVal());
-		VEDArbin<Integer> arbol3=new VEDArbin<Integer>(30,vacio,vacio);
-		System.out.println(arbol3.getVal());
-		System.out.println(arbol1.esVacio());
-		System.out.println(arbol1.peso());
-		System.out.println(arbol1.altura());
+		VEDArbin<Integer> vacio = new VEDArbin<Integer>();
+		VEDArbin<Integer> raiz = new VEDArbin<Integer>(10,vacio,vacio);
+		VEDArbin<Integer> derecho = new VEDArbin<Integer>(10,vacio,raiz);
+		VEDArbin<Integer> izquierdo = new VEDArbin<Integer>(10,raiz,derecho);
+		
+		System.out.println(raiz.getVal());
+		System.out.println(derecho.getDer());
+		System.out.println(izquierdo.getIzq());
+		System.out.println(vacio.peso());
 	}
 }
